1fa6dc46afec9d163b477a1265a17ac33bd6c550
Bhavin Kamani <13havinkamani@gmail.com>
2022-12-22 11:12:12 +0530

    Add `zip-webpack-plugin` to bundle the dist folder.
    Signed-off-by: bhavin-kamani <bhavin.kamani@noovosoft.com>

--- webpack.distribution.js
+++ webpack.distribution.js
@@ -1,10 +1,18 @@
 const {merge} = require("webpack-merge");
-const common = require("/webpack.common")
+const common = require("./webpack.common")
+const ZipPlugin = require("zip-webpack-plugin")
+const path = require("path");
 
 
 module.exports = () => {
     return merge(common(), {
         mode: "production",
+        plugins: [
+            new ZipPlugin({
+                path: path.resolve("bundles"),
+                filename: "magically.zip"
+            })
+        ],
         output: {
             filename: "[name].js"
         }
