9d7e3e8147de18607729ab11035331490a6da256
Bhavin Kamani <13havinkamani@gmail.com>
2022-12-30 07:48:47 +0530

    Add a `obfuscation` step in building the dist zip.
    Signed-off-by: bhavin-kamani <bhavin.kamani@noovosoft.com>

--- webpack.distribution.js
+++ webpack.distribution.js
@@ -2,12 +2,34 @@ const {merge} = require("webpack-merge");
 const common = require("./webpack.common")
 const ZipPlugin = require("zip-webpack-plugin")
 const path = require("path");
+const WebpackObfuscatorPlugin = require("webpack-obfuscator");
 
 
 module.exports = () => {
     return merge(common(), {
         mode: "production",
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: [
+                        /node_modules/,
+                    ],
+                    enforce: 'post',
+                    use: {
+                        loader: WebpackObfuscatorPlugin.loader,
+                        options: {
+                            rotateStringArray: true
+                        }
+                    }
+                }
+            ],
+        },
         plugins: [
+            new WebpackObfuscatorPlugin({
+                    rotateStringArray: true,
+                },
+            ),
             new ZipPlugin({
                 path: path.resolve("bundles"),
                 filename: "magically.zip"
